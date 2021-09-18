57cf790b6a4664a01a37bbcc732ed723d12277c5
YMekki <90990708+YMekki@users.noreply.github.com>
2021-09-18 18:26:10 -0500

    Change Title

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mekk v1
+          Lol First THING 
         </p>
         <a
           className="App-link"
